5dc328166e12ca9c9d37a47b516a1160f68333fd
Eva <evangeline013@gmail.com>
2018-10-02 16:53:20 -0400

    Add gameOver function and losing condition

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let openDoor1;
 let openDoor2;
 let openDoor3;
 let numClosedDoors = 3;
+let startButton = document.getElementById('start');
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
@@ -28,6 +29,12 @@ const randomChoreDoorGenerator = () => {
   }
 }
 
+const isBot = (door) => {
+  if (door.src == botDoorPath) {
+    return true;
+  }
+  return false;
+}
 
 const isClicked = (door) => {
   if (door.src == closedDoorPath) {
@@ -36,9 +43,11 @@ const isClicked = (door) => {
   return true;
 }
 
-const playDoor = () => {
+const playDoor = (door) => {
   numClosedDoors -= 1;
   if (numClosedDoors == 0) {
+    gameOver("win");
+  } else if (isBot(door)) {
     gameOver();
   }
 }
@@ -46,20 +55,28 @@ const playDoor = () => {
 doorImage1.onclick = () => {
   if (!isClicked(doorImage1)) {
     doorImage1.src = openDoor1;
-    playDoor();
+    playDoor(doorImage1);
   }
 };
 
 doorImage2.onclick = () => {
   if (!isClicked(doorImage2)) {
     doorImage2.src = openDoor2;
-    playDoor();
+    playDoor(doorImage2);
   }
 };
 
 doorImage3.onclick = () => {
   if (!isClicked(doorImage3)) {
     doorImage3.src = openDoor3;
-    playDoor();
+    playDoor(doorImage3);
   }
 };
+
+const gameOver = (status) => {
+  if (status == "win") {
+    startButton.innerHTML = "You win! Play again?";
+  } else {
+    startButton.innerHTML = "Gave over! Play again?";
+  }
+}
